9cf5a08e3e54c9091e22476217e4d25b57e50407
rrouz <ryinrouz94@gmail.com>
2024-04-03 16:27:27 -0700

    Improved FASTA header processing

--- scripts/seq_prep.py
+++ scripts/seq_prep.py
@@ -7,15 +7,26 @@ input_file = "protein_sequences.fasta"
 output_file = "alignments/modified_protein_sequences.fasta"
 
 with open(input_file, "r") as infile, open(output_file, "w") as outfile:
-            header = ""
-            sequence = ""
-            for line in infile:
-                if line.startswith(">"):
-                    if header:
-                        processed_header = header.split("|")[0] + "|" + header.split("|")[1] + "|" + ".".join(header.split("|")[2].split(".")[:2])
-                        outfile.write(f">{processed_header}\n{sequence}\n")
-                    header = line[1:].replace("_", ".").replace(" ", ".").replace(":", "-").replace("/", ".").replace('"', ".").replace("-", ".")
-                    sequence = ""
-                else:
-                    sequence += line.strip()
-                    
+    header = ""
+    sequence = ""
+    for line in infile:
+        if line.startswith(">"):
+            if "sp|" not in line[1:4]:
+                line = line[:1] + "sp|" + line[1:]
+            if header:
+                processed_header = header.split("|")[0] + "|" + header.split("|")[1] + "|" + ".".join(
+                    header.split("|")[2].split(".")[:2]
+                )
+                outfile.write(f">{processed_header}\n{sequence}\n")
+                header = ""
+                sequence = "" 
+            header = line[1:].replace("_", ".").replace(" ", ".").replace(":", "-").replace("/", ".").replace(
+                '"', ".").replace("-", ".")
+        else:
+            sequence += line.strip()
+
+    if header:
+        processed_header = header.split("|")[0] + "|" + header.split("|")[1] + "|" + ".".join(
+            header.split("|")[2].split(".")[:2]
+        )
+        outfile.write(f">{processed_header}\n{sequence}\n")
